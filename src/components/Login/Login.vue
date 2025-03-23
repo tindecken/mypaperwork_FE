c751e82281242a3a28bc151386762de24d6a4b3d
Tindecken <tindecken@gmail.com>
2025-03-23 16:38:50 +0700

    feat: add Google login option and registration prompt in Login component

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -9,6 +9,10 @@
             <q-btn label="Login" type="submit" color="primary" />
           </div>
         </q-form>
+        <q-separator class="q-mt-md q-mb-xs"></q-separator>
+        <q-btn label="Login with Google Account" @click="handleLoginGoogle"></q-btn>
+        <q-separator class="q-mt-md q-mb-xs"></q-separator>
+        <span class="text-subtitle1">Don't have account? Register one here.</span>
       </div>
     </q-page-container>
     <q-footer reveal bordered class="bg-primary text-white" style="height: 24px"> Tindecken @ 2023 - Quasar: {{ $q.version }} </q-footer>
@@ -63,6 +67,13 @@ const onLogout = async () => {
       message: error.message || 'Registration failed',
     });
   }
+};
+const handleLoginGoogle = async () => {
+  const data = await authClient.signIn.social({
+    provider: 'google',
+  });
+
+  return data;
 };
 const onClickLogin = async () => {
   $q.loading.show({
