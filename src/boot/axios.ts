147e2f36578dc6fa0c8c2163c79c6963f0ddb559
hnu <hnu@sucafina.com>
2025-06-05 15:42:54 +0700

    feat: implement axios and auth client configuration with API base URL setup

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -17,7 +17,7 @@ declare module '@vue/runtime-core' {
 // "export default () => {}" function below (which runs individually
 // for each client)
 
-const api = axios.create({ baseURL: process.env.API_BASEURL });
+const api = axios.create({ baseURL: process.env.API_BASEURL + '/api' });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from 'better-auth/vue';
 export const authClient = createAuthClient({
-  baseURL: process.env.API_BASEURL || 'http://localhost:3001', // the base url of your auth server
+  baseURL: process.env.API_BASEURL, // the base url of your auth server
 });
