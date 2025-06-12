b37c37e0f38ebd4d216620e56c05ac7acdfe6b5b
Tindecken <tindecken@gmail.com>
2025-06-12 20:40:56 +0700

    fixing

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from 'better-auth/vue';
 export const authClient = createAuthClient({
-  baseURL: process.env.API_BASEURL, // the base url of your auth server
+  baseURL: process.env.API_BASEURL + '/api', // the base url of your auth server
 });
