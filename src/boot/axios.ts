3841bd19c5292f993ed2647331a66c81a391b92e
Tindecken <tindecken@gmail.com>
2025-06-12 20:51:52 +0700

    fixing

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -14,7 +14,7 @@ declare module '@vue/runtime-core' {
 // "export default () => {}" function below (which runs individually
 // for each client)
 
-const api = axios.create({ baseURL: process.env.API_BASEURL + '/api' });
+const api = axios.create({ baseURL: process.env.API_BASEURL });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
