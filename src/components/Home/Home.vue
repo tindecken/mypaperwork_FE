be358b4830d318b862a5d67e7dba60349b84936e
hnu <hnu@sucafina.com>
2025-08-07 17:05:58 +0700

    feat: move loading indicator for paperworks retrieval to onMounted lifecycle hook

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -43,21 +43,6 @@ const userStore = useUserStore();
 const $q = useQuasar();
 const paperworkStore = usePaperworkStore();
 
-$q.loading.show({
-  message: 'Getting paperworks...',
-});
-paperworkStore
-  .getPaperworks()
-  .then(() => {
-    $q.loading.hide();
-  })
-  .catch((err: GenericResponseData | any) => {
-    $q.loading.hide();
-    $q.notify({
-      type: 'negative',
-      message: err.message || err.title || err,
-    });
-  });
 onBeforeMount(() => {
   themeStore.getThemes();
 });
@@ -82,6 +67,22 @@ onMounted(async () => {
       userStore.$patch({
         userInfo: userInfo,
       });
+
+      $q.loading.show({
+        message: 'Getting paperworks...',
+      });
+      paperworkStore
+        .getPaperworks()
+        .then(() => {
+          $q.loading.hide();
+        })
+        .catch((err: GenericResponseData | any) => {
+          $q.loading.hide();
+          $q.notify({
+            type: 'negative',
+            message: err.message || err.title || err,
+          });
+        });
     }
   } catch (error: any) {
     $q.loading.hide();
